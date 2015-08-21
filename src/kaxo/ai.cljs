1a6e7fc33ae310f193d93dfb677a4041d0e99ce5
gmp26 <gmp26@cam.ac.uk>
2015-08-21 08:29:19 +0100

    better disjoint region algorithm

--- src/kaxo/ai.cljs
+++ src/kaxo/ai.cljs
@@ -1,17 +1,13 @@
 (ns ^:figwheel-always kaxo.ai
   "ai game play for kaxo"
   (:require [kaxo.core :as k]
-            [clojure.set :refer (difference)])
-  )
-
-(defonce partitions #{})
+            [clojure.set :refer (union difference)]))
 
 (defn initial-dots
   "calculate initial dots in game"
   [n]
   (let [rng (range n)]
-    (into #{} (for [x rng y rng] [x y])))
-  )
+    (into #{} (for [x rng y rng] [x y]))))
 
 (defn remaining-dots
   "find remaining dots in game"
@@ -23,36 +19,34 @@
   [wps line]
   (not-any? (k/new-way-points (k/canonical-line line)) wps))
 
-(defn adjacent?
- "are 2 dots adjacent?"
- [wps [x1 y1 :as dot1] [x2 y2 :as dot2]]
- (let [line [dot1 dot2]
-       result (and (<= (Math.abs (- x1 x2)) 1)
-                   (<= (Math.abs (- y1 y2)) 1)
-                   (not= dot1 dot2)
-                   (linkable? wps line))]
-   (k/deb (str line) result))
- )
+(defn get-boundary
+  "find boundary of a neighbourhood around a new neighbouring dot within an area"
+  [area wps neighbours [x y :as dot]]
+  (let [yet-to-explore (difference area neighbours)]
+    #_(prn "yet-to-explore " yet-to-explore)
+    (into #{}
+          (for [x' (range (- x 1) (+ x 2))
+                y' (range (- y 1) (+ y 2))
+                :let [dot' [x' y']]
+                :when (and (not= dot' dot)
+                           (yet-to-explore dot')
+                           (linkable? wps (k/canonical-line [dot' dot])))]
+            dot'))))
 
-(defn set-adjacent?
-  "determine whether a dot is adjacent to any dots in a given set"
-  [wps neighbours dot]
-  (some #(adjacent? wps % dot) neighbours))
-
-(defn connected
-  "the dots in the connected neighbourhood of a given dot"
-  [wps dot remaining]
+(defn grow-connected-region-in-area
+  "grow a neighbourhood from a seed"
+  [area wps dot]
   (loop [neighbours #{dot}
-         remotes remaining]
-    (prn (str "neighbours " neighbours))
-    (prn (str "remotes " remotes))
-    (if (empty? remotes)
+         boundary (get-boundary area wps neighbours dot)]
+    (prn "  neighbours " neighbours)
+    (prn "  boundary " boundary)
+    (if (empty? boundary)
       neighbours
-      (let [candidate (first remotes)
-            remainder (rest remotes)]
-        (if (set-adjacent? wps neighbours candidate)
-          (recur (conj neighbours candidate) remainder)
-          (recur neighbours remainder))))))
+      (let [candidate (first boundary)
+            remainder (into #{} (rest boundary))]
+        (recur (conj neighbours candidate)
+               (union remainder
+                      (get-boundary area wps neighbours candidate)))))))
 
 (defn regions
   "return all connected regions on game board"
@@ -60,19 +54,14 @@
   (let [space (remaining-dots n wps)]
     (loop [regions []
            remaining space]
-      (prn (str "regions " regions))
-      (prn (str "remaining " remaining))
+      (prn "regions " regions)
+      (prn "remaining " remaining)
       (if (empty? remaining)
         regions
-        (let [dot (k/deb "dot " (first (seq remaining)))
-              foo (k/deb "foo remaining " remaining)
-              region (k/deb "let reg " (connected wps dot remaining))
+        (let [dot (first remaining)
+              region (grow-connected-region-in-area remaining wps dot)
               space-left (difference remaining region)]
-          (recur (conj regions region) space-left)
-          ))
-      ))
-  )
-
+          (recur (conj regions region) space-left))))))
 
 (defn get-ai-move
   "plan an ai move"
